9c91f35867f2662d4adc5399691f54085c238c26
hanxj <dawnjeanh@gmail.com>
2019-08-02 07:42:08 +0800

    add code

---
+++ project/src/run_vo.cpp
@@ -0,0 +1,10 @@
+#include "common.hpp"
+
+int main(int argc, char **agrv)
+{
+    if (argc != 2)
+    {
+        std::cout << "usage: run_vo parameter_file" << std::endl;
+        return 1;
+    }
+}
